0366ec9836ad3b7c673233dc14db2c48f9a43f5d
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2025-10-12 23:11:35 -0400

    Initial commit

---
+++ songs/turnTimer.hx
@@ -0,0 +1,64 @@
+import funkin.editors.charter.Charter;
+import objects.TurnTimerSprite;
+
+public var turnTimer:TurnTimerSprite;
+var earliestPoint:Float = PlayState.chartingMode && Charter.startHere ? Charter.startHere : 0;
+var latestPoint:Float = 0;
+
+function postCreate():Void {
+	turnTimer = new TurnTimerSprite();
+	turnTimer.visible = !PlayState.coopMode;
+	turnTimer.screenCenter(FlxAxes.X);
+	turnTimer.cameras = [camHUD];
+	turnTimer.y = 60;
+	add(turnTimer);
+	updateTiming();
+}
+
+function postUpdate(elapsed:Float):Void {
+	if (turnTimer.visible) {
+		var gapTooSmall = latestPoint - earliestPoint <= Conductor.crochet * Conductor.beatsPerMeasure;
+		if (!gapTooSmall) turnTimer.percent = FlxMath.bound(FlxMath.remapToRange(inst.time, earliestPoint, latestPoint, 0, 1), 0, 1);
+		turnTimer.alpha = lerp(turnTimer.alpha, gapTooSmall ? 0 : 1, 0.15);
+	}
+}
+
+function updateTiming(?note:Note):Void {
+	if (turnTimer.visible) {
+		var timeOffset:Float = Conductor.stepCrochet * (Conductor.beatsPerMeasure / 2);
+		if (note == null) {
+			for (strumLine in strumLines)
+				if (strumLine.opponentSide == PlayState.opponentMode)
+					for (note in strumLine.notes)
+						if (!note.isSustainNote && note.strumTime > earliestPoint)
+							latestPoint = Math.max(note.strumTime + getSustainLength(note) - timeOffset, 0);
+		} else if (!note.isSustainNote && note.strumLine.opponentSide == PlayState.opponentMode) {
+			earliestPoint = note.strumTime + getSustainLength(note);
+			for (note in note.strumLine.notes)
+				if (!note.isSustainNote && note.strumTime > earliestPoint)
+					latestPoint = Math.max(note.strumTime + getSustainLength(note) - timeOffset, 0);
+		}
+	}
+}
+
+function onNoteHit(event):Void {
+	updateTiming(event.note);
+}
+function onPlayerMiss(event):Void {
+	updateTiming(event.note);
+}
+
+function getSustainLength(note:Note):Float {
+	var length:Float = 0;
+	sustainLoop(note.nextNote, (sustain:Note) -> length += sustain.sustainLength, true);
+	return length;
+}
+function sustainLoop(note:Note, func:Note->Void, ?noEffectParent:Bool):Void {
+	var aNote:Note = note;
+	noEffectParent ??= false;
+	while (aNote != null) {
+		if (noEffectParent ? aNote != note : true)
+			func(aNote);
+		aNote = aNote.nextSustain;
+	}
+}
---
+++ source/objects/TurnTimerSprite.hx
@@ -0,0 +1,22 @@
+class TurnTimerSprite extends FunkinSprite {
+	public var circleShader:CustomShader;
+
+	public var percent(get, set):Float;
+	function get_percent():Float
+		return circleShader.percent;
+	function set_percent(value:Float):Float
+		return circleShader.percent = value;
+
+	public function new(?isPixel:Bool) {
+		isPixel ??= false;
+		super(0, 0, Paths.image('dial-sprite' + (isPixel ? '-pixel' : '')));
+		setGraphicSize(50, 50);
+		scale.x *= 1.7;
+		scale.y *= 1.7;
+		updateHitbox();
+		circleShader = new CustomShader('circle');
+		circleShader.percent = 0;
+		shader = circleShader;
+		antialiasing = !isPixel;
+	}
+}
