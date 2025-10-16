12a679b3bd78ef28c42aeaa3dfa52c8e2a3aff96
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2025-10-15 21:12:48 -0400

    Update turnTimer.hx
    * gapTooSmall calculations are now perfected (not counting BPM changes)
    * Fixed negative numbers occuring on jumps (doubles).
    * Prevented the "point" vars from bring set to an earlier point in time to account for holding a sustain while pressing other notes.

--- songs/turnTimer.hx
+++ songs/turnTimer.hx
@@ -4,6 +4,9 @@ import objects.TurnTimerSprite;
 public var turnTimer:TurnTimerSprite;
 var earliestPoint:Float = PlayState.chartingMode && Charter.startHere ? Charter.startHere : 0;
 var latestPoint:Float = 0;
+// would be doing "set_" but that just doesn't work for some reason
+function greaterTimeCheck(oldTime:Float, newTime:Float):Float
+	return newTime > oldTime ? newTime : oldTime;
 
 function postCreate():Void {
 	turnTimer = new TurnTimerSprite();
@@ -17,7 +20,15 @@ function postCreate():Void {
 
 function postUpdate(elapsed:Float):Void {
 	if (turnTimer.visible) {
-		var gapTooSmall = latestPoint - earliestPoint <= Conductor.crochet * Conductor.beatsPerMeasure;
+		var calculatedStepDuration:Float = (latestPoint - earliestPoint) / Conductor.stepCrochet; // step count between points
+		var stepsPerMeasure:Float = Conductor.beatsPerMeasure * Conductor.stepsPerBeat; // total steps per measure
+		var stepOffset:Float = 0.7; // shortens "stepsPerMeasure" amount by said amount of steps
+		trace(
+			calculatedStepDuration,
+			Math.max(stepsPerMeasure - stepOffset, 0), // Math.max used to prevent negative numbers
+			calculatedStepDuration < Math.max(stepsPerMeasure - stepOffset, 0) // final math
+		);
+		var gapTooSmall = calculatedStepDuration < Math.max(stepsPerMeasure - stepOffset, 0);
 		if (!gapTooSmall) turnTimer.percent = FlxMath.remapToRange(inst.time, earliestPoint, latestPoint, 0, 1);
 		turnTimer.alpha = lerp(turnTimer.alpha, gapTooSmall ? 0 : 1, 0.15);
 	}
@@ -25,20 +36,26 @@ function postUpdate(elapsed:Float):Void {
 
 function updateTiming(?note:Note):Void {
 	if (turnTimer.visible) {
-		var timeOffset:Float = Conductor.stepCrochet * (Conductor.beatsPerMeasure / 2);
 		if (note == null) {
+			var newTime:Float = 0;
 			for (strumLine in strumLines)
 				if (strumLine.opponentSide == PlayState.opponentMode)
 					for (note in strumLine.notes)
 						if (!note.isSustainNote && note.strumTime > earliestPoint) // unsure if putting a break is needed for unnecessary looping, will look into later
-							latestPoint = Math.max(note.strumTime - timeOffset, 0);
+							newTime = note.strumTime;
+			latestPoint = greaterTimeCheck(latestPoint, newTime);
 		} else if (!note.isSustainNote && note.strumLine.opponentSide == PlayState.opponentMode) {
-			earliestPoint = note.strumTime + getSustainLength(note);
+			earliestPoint = greaterTimeCheck(earliestPoint, note.strumTime + getSustainLength(note));
 			if (note.nextNote.isSustainNote) {
 				var lastNote:Note;
-				sustainLoop(note.nextNote, (sustain:Note) -> lastNote = sustain.nextNote);
-				latestPoint = Math.max(lastNote == null ? earliestPoint : lastNote.strumTime - timeOffset, 0);
-			} else latestPoint = Math.max(note.nextNote.strumTime - timeOffset, 0);
+				function timeCheck(saidNote:Note) {
+					sustainLoop(saidNote.nextNote, (sustain:Note) -> lastNote = sustain.nextNote);
+					if (lastNote != null && lastNote.strumTime == saidNote.strumTime)
+						timeCheck(lastNote);
+				}
+				timeCheck(note);
+				latestPoint = lastNote == null ? earliestPoint : greaterTimeCheck(latestPoint, lastNote.strumTime);
+			} else latestPoint = greaterTimeCheck(latestPoint, note.nextNote.strumTime);
 		}
 	}
 }
